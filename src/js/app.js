11a601965a7289b84a73a68db6270e7970cc1af8
Natalia <nata@host>
2021-10-30 12:33:55 +0500

    lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,24 @@
-export const id = setInterval(sec, 1000);
-id;
 const r = document.querySelectorAll('.cell');
 const elem = document.createElement('img');
-//const li = document.createElement('li');
+// const li = document.createElement('li');
 elem.src = 'https://raw.githubusercontent.com/netology-code/ahj-homeworks/simplification/dom/pic/goblin.png';
 elem.alt = 'рожица';
 elem.setAttribute('data', 'location');
 
-function getRandomIntInclusive(min = 0, max = 15) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function sec() {
-    const i = getRandomIntInclusive();
-    r[i].appendChild(elem);
+  const i = getRandomIntInclusive(0, 15);
+  r[i].appendChild(elem);
 }
+
+/* export default function id() {
+  setInterval(sec, 1000);
+} */
+
+export const id = setInterval(sec, 1000);
+// id;
--- src/js/eventHandling.js
+++ src/js/eventHandling.js
@@ -1,15 +1,16 @@
 import { id } from './app';
+
 const father = document.querySelector('.pole');
-let score = 0;
+// let score = 0;
 let away = 0;
 
 father.addEventListener('click', (event) => {
-    let elementOk = event.target.getAttribute('data');
-    if (elementOk == 'location') {
-        score++;
-    } else away++;
-    if (away == 5) {
-        clearTimeout(id);
-        alert('Игра окончина!');
-    }
+  const elementOk = event.target.getAttribute('data');
+  if (elementOk !== 'location') {
+    away += 1;
+  }
+  if (away === 5) {
+    clearTimeout(id);
+    alert('Игра окончина!');
+  }
 });
